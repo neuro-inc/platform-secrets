e0dce7396b9208228cdd8d009a4feba3a87c0704
Aleksandr Danshyn <dalazx@gmail.com>
2020-06-05 17:11:33 +0300

    Added some validators

---
+++ platform_secrets/validators.py
@@ -0,0 +1,24 @@
+import base64
+
+import trafaret as t
+
+
+SECRET_KEY_PATTERN = r"\A[a-zA-Z0-9_\-\.]+\Z"
+
+
+def check_base64(value: str) -> str:
+    try:
+        base64.b64decode(value)
+    except Exception as exc:
+        raise t.DataError(str(exc))
+    return value
+
+
+secret_key_validator = t.String(min_length=1, max_length=253) & t.Regexp(
+    SECRET_KEY_PATTERN
+)
+secret_value_validator = t.String(max_length=1024 * 1024) >> check_base64
+secret_request_validator = t.Dict(
+    {"key": secret_key_validator, "value": secret_value_validator}
+)
+secret_response_validator = t.Dict({"key": t.String})
---
+++ tests/unit/test_validators.py
@@ -0,0 +1,69 @@
+import pytest
+import trafaret as t
+
+from platform_secrets.validators import (
+    secret_request_validator,
+    secret_response_validator,
+)
+
+
+@pytest.mark.parametrize("key", ["", "?", "\n", "\t", " "])
+def test_secret_request_validator__invalid_key(key: str) -> None:
+    validator = secret_request_validator
+    payload = {"key": key, "value": "vvvv"}
+    with pytest.raises(t.DataError):
+        validator.check(payload)
+
+
+def test_secret_request_validator__invalid_key_too_long() -> None:
+    key = "a" * 254
+    validator = secret_request_validator
+    payload = {"key": key, "value": "vvvv"}
+    with pytest.raises(t.DataError):
+        validator.check(payload)
+
+
+@pytest.mark.parametrize("key", ["a", ".", "-", "_", "0", "A"])
+def test_secret_request_validator__valid_key(key: str) -> None:
+    validator = secret_request_validator
+    payload = {"key": key, "value": "vvvv"}
+    result = validator.check(payload)
+    assert result == payload
+
+
+def test_secret_request_validator__valid_key_max_len() -> None:
+    key = "a" * 253
+    validator = secret_request_validator
+    payload = {"key": key, "value": "vvvv"}
+    result = validator.check(payload)
+    assert result == payload
+
+
+def test_secret_request_validator__invalid_value_base64() -> None:
+    validator = secret_request_validator
+    payload = {"key": "testkey", "value": "vvvvv"}
+    with pytest.raises(t.DataError, match="Invalid base64-encoded string"):
+        validator.check(payload)
+
+
+def test_secret_request_validator__valid_value_max_len() -> None:
+    value = "v" * 1024 * 1024
+    validator = secret_request_validator
+    payload = {"key": "testkey", "value": value}
+    result = validator.check(payload)
+    assert result == payload
+
+
+def test_secret_request_validator__invalid_value_too_long() -> None:
+    value = "v" * (1024 * 1024 + 1)
+    validator = secret_request_validator
+    payload = {"key": "testkey", "value": value}
+    with pytest.raises(t.DataError):
+        validator.check(payload)
+
+
+def test_secret_response_validator__long_key() -> None:
+    validator = secret_response_validator
+    payload = {"key": "k" * 255}
+    result = validator.check(payload)
+    assert result == payload
