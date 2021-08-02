3832d65c72492fcd3c637b626b80db128db72cb4
Andrew Svetlov <andrew.svetlov@gmail.com>
2021-08-02 12:02:53 +0300

    Switch from platform-logging to neuro-logging
    Committed via https://github.com/asottile/all-repos

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ install_requires = (
     "multidict==5.1.0",
     "neuro_auth_client==21.6.15",
     "trafaret==2.1.0",
-    "platform-logging==21.7.27",
+    "neuro-logging==21.8.2.2",
     "aiohttp-cors==0.7.0",
     "aiozipkin==1.1.0",
     "sentry-sdk==1.3.0",
